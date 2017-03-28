68a8134aa37af520b074cc7b07e80201a6e6f9b9
Matt Pedersen <mpedersen@mindrocketdesign.com>
2017-03-27 21:08:36 -0600

    fixed auth routes (missing commas)

--- assets/app/authentication/auth.routes.ts
+++ assets/app/authentication/auth.routes.ts
@@ -4,9 +4,9 @@ import { LoginComponent } from './login.component';
 import { LogoutComponent } from './logout.component';
 
 export const AUTH_ROUTES: Routes = [
-	{ path: '', redirectTo: 'signup', pathMatch: 'full' }
-	{ path: 'signup', component: SignupComponent }
-	{ path: 'login', component: LoginComponent }
+	{ path: '', redirectTo: 'signup', pathMatch: 'full' },
+	{ path: 'signup', component: SignupComponent },
+	{ path: 'login', component: LoginComponent },
 	{ path: 'logout' , component: LogoutComponent }
 ];
 
