6b07d9b777be87055d232a048d65bd837c385365
Matt Pedersen <mpedersen@mindrocketdesign.com>
2017-03-27 22:36:37 -0600

    change the authentication route to just 'auth'

--- assets/app/app.routes.ts
+++ assets/app/app.routes.ts
@@ -5,7 +5,7 @@ import { AUTH_ROUTES } from './authentication/auth.routes.ts';
 const APP_ROUTES: Routes = [
 	{path:'', redirectTo:'/messages', pathMatch: 'full'},
 	{path:'messages', component: MessagesComponent},
-	{path:'authentication', component: AuthenticationComponent, children: AUTH_ROUTES }
+	{path:'auth', component: AuthenticationComponent, children: AUTH_ROUTES }
 ];
 
 export const myRoutes = RouterModule.forRoot(APP_ROUTES);
