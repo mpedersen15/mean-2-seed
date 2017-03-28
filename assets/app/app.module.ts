b4a5c5c85a51802619fd5c9045bf27ae2ef44a8b
Matt Pedersen <mpedersen@mindrocketdesign.com>
2017-03-27 18:38:17 -0600

    added Header, Auth, and Messages components

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from "./app.component";
 import { MessageComponent } from "./messages/message.component";
+import { MessagesComponent } from "./messages/messages.component";
 import { MessageListComponent } from "./messages/message-list.component";
 import { MessageInputComponent } from "./messages/message-input.component";
 import { MessageService } from "./messages/message.service";
-
+import { HeaderComponent } from "./header.component";
 @NgModule({
     declarations: [
         AppComponent,
 		MessageComponent,
 		MessageListComponent,
-		MessageInputComponent
+		MessageInputComponent,
+		MessagesComponent,
+		HeaderComponent
     ],
     imports: [BrowserModule, FormsModule],
     bootstrap: [AppComponent],
---
+++ assets/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+
+const APP_ROUTES: Routes = [
+	{'messages', component: MessagesComponent},
+	{'authentication', component: AuthenicationComponent},
+]
